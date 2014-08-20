fd0940c084e7e29db7dab7cbbb434f9c224d5e4f
Liang Sun <i@liangsun.org>
2014-08-20 19:42:50 +0800

    Remove home logic.

--- controller/__init__.py
+++ controller/__init__.py
@@ -14,29 +14,16 @@ class AdminHandler(BaseHandler):
 class PageEditHandler(BaseHandler):
     def get(self, url):
         page = self.db.page.find_one({'url': url}) or {'url': url}
-        site = self.db.site.find_one()
-        home = site and site.get('home', '_') or '_'
-        if home == url:
-            page['home'] = True
-
         self.render('_page_form.html', page=page)
 
     def post(self, url):
         page = self.db.page.find_one({'url': url}) or {'url': url}
         title = self.get_argument('title')
         content = self.get_argument('content')
-        home = self.get_argument('home', 'false') == 'true'
 
         page['title'] = title
         page['content'] = content
         self.db.page.save(page)
-
-        site = self.db.site.find_one() or {}
-        if home:
-            site['home'] = url
-        else:
-            site.pop('home', None)
-        self.db.site.save(site)
 
         self.write({'ok':1})
 
